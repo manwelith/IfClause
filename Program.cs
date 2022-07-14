853470a3d2c4d4694bbd2813cafd557949f439ca
Soner A <107695479+manwelith@users.noreply.github.com>
2022-07-14 15:32:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        if (N >= 1 && N <= 100)
+        {
+            if (N % 2 == 1 || (N >= 6 && N <= 20))
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+
+        Console.ReadLine();
+    }
+}
